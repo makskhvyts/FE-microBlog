080ea4d7b2284ecc63a80779bd5569672b5d7a6d
repo <khvits21@gmail.com>
2024-12-23 23:12:22 +0200

    Add like/unlike post functionality with state toggle

---
+++ src/stores/actions/posts/useLikePost.js
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import useApiRequest from "../../../hooks/useApiRequest";
+
+const useLikePost = (setIsLiked) => {
+  const axios = useApiRequest();
+
+  const likePost = async ({ postId, authorUsername }) => {
+    const { data } = await axios.put(`users/${authorUsername}/posts/${postId}/like`);
+    return data;
+  };
+
+  const unlikePost = async ({ postId, authorUsername }) => {
+    const { data } = await axios.delete(`users/${authorUsername}/posts/${postId}/like`);
+    return data;
+  };
+
+  return useMutation({
+    mutationKey: ["like"],
+    mutationFn: async ({ postId, authorUsername, isLiked }) => {
+      return isLiked
+        ? await unlikePost({ postId, authorUsername })
+        : await likePost({ postId, authorUsername });
+    },
+    onSuccess: (_, variables) => {
+      setIsLiked(!variables.isLiked);
+    },
+  });
+};
+
+export default useLikePost;

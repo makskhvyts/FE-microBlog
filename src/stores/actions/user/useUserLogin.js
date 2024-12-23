9499c26e4bb7b530dcd42dfc9cf7cfe91283a9d1
repo <khvits21@gmail.com>
2024-12-23 23:33:04 +0200

    fix bugs for useUserLogin

---
+++ src/stores/actions/user/useUserLogin.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import useApiRequest from "../../../hooks/useApiRequest";
+import useUserAuth from "../../../hooks/useUserAuth";
+
+const useUserLogin = () => {
+  const axios = useApiRequest();
+  const navigate = useNavigate();
+  const { login } = useUserAuth();
+  
+  return useMutation({
+    mutationKey: ["login"],
+    mutationFn: async ({ username, password }) => {
+      const { data } = await axios.post(`/login`, {
+        username,
+        password,
+      });
+      return data;
+    },
+    onSuccess: (data, variables) => {
+      login(variables.username, variables.password);
+      navigate("/");
+    },
+  });
+};
+
+export default useUserLogin;

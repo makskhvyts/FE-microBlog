856e7c85ad40a268f220d6bf0adaa495485231fb
repo <khvits21@gmail.com>
2024-12-23 23:11:43 +0200

    Add usePostCreation hook with mutation and refetch logic

---
+++ src/stores/actions/posts/usePostCreation.js
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import useApiRequest from "../../../hooks/useApiRequest";
+import useUserAuth from "../../../hooks/useUserAuth";
+
+const usePostCreation = (refetchPosts) => {
+  const axios = useApiRequest();
+  const { username } = useUserAuth();
+
+  const createPost = async (content) => {
+    const { data } = await axios.post(`users/${username}/posts`, { content });
+    return data;
+  };
+
+  return useMutation({
+    mutationKey: ["newPost"],
+    mutationFn: ({ content }) => createPost(content),
+    onSuccess: () => {
+      refetchPosts();
+    },
+  });
+};
+
+export default usePostCreation;

f23895f4f6dc5154ce3e2c0676fffe87ff0f03d5
repo <khvits21@gmail.com>
2024-12-23 23:15:31 +0200

    Add full name input with validation error handling

---
+++ src/pages/compLogin/RegisterForm.js
@@ -0,0 +1,25 @@
+import LoginForm from "./LoginForm";
+import InputField from "./InputField";
+
+const RegisterForm = ({ username, password, fullName, onChange, isError }) => {
+  return (
+    <>
+      <LoginForm
+        username={username}
+        password={password}
+        onChange={onChange}
+        isError={isError}
+      />
+      <InputField
+        id="full_name"
+        label="Full Name"
+        type="text"
+        value={fullName}
+        onChange={(value) => onChange({ full_name: value })}
+        isError={isError}
+      />
+    </>
+  );
+};
+
+export default RegisterForm;
